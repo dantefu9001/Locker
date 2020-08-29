89059be4a2350cb5b247dcdd2b5f0fb179ecf73e
zenan <zenan.fu@thoughtworks.com>
2020-08-29 09:12:20 +0800

    should not open locker when having no empty lockers and given a request to store package

--- src/main/java/cn/xpbootcamp/locker/Locker.java
+++ src/main/java/cn/xpbootcamp/locker/Locker.java
@@ -2,8 +2,23 @@ package cn.xpbootcamp.locker;
 
 public class Locker {
 
+    private final boolean IS_FULL = true;
+
+    private boolean status;
+
     public Receipt openEmptyLocker() {
-        return new Receipt();
+        if (status == IS_FULL) {
+            return null;
+        } else {
+            return new Receipt();
+        }
     }
 
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
--- src/test/java/cn/xpbootcamp/locker/LockerTest.java
+++ src/test/java/cn/xpbootcamp/locker/LockerTest.java
@@ -5,10 +5,21 @@ import org.junit.Test;
 
 public class LockerTest {
 
+    private static final boolean IS_FULL = true;
+
     @Test
     public void should_open_an_empty_locker_given_request_to_store_when_having_empty_lockers(){
         Locker locker = new Locker();
+        locker.setStatus(!IS_FULL);
         Receipt receipt = locker.openEmptyLocker();
         Assert.assertNotNull(receipt);
+    }
+
+    @Test
+    public void should_not_open_a_locker_given_request_to_store_when_having_no_empty_lockers(){
+        Locker locker = new Locker();
+        locker.setStatus(IS_FULL);
+        Receipt receipt = locker.openEmptyLocker();
+        Assert.assertNull(receipt);
     }
 }
