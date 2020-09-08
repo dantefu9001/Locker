672eccc1617314daab30a5bc292875642f1f04ec
zenan <zenan.fu@thoughtworks.com>
2020-09-08 23:01:40 +0800

    should fail when store bag by himself given no empty locker

--- src/test/java/cn/xpbootcamp/locker/LockerBotManagerTest.java
+++ src/test/java/cn/xpbootcamp/locker/LockerBotManagerTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Collections;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 public class LockerBotManagerTest {
     @Test
@@ -12,5 +13,13 @@ public class LockerBotManagerTest {
         Locker lockerC = new Locker(10);
         LockerBotManager lockerBotManager = new LockerBotManager(Collections.singletonList(lockerC));
         assertNotNull(lockerBotManager.storeBag());
+    }
+
+    @Test
+    public void should_fail_when_saving_bags_given_lockers_with_no_capacity(){
+        Locker locker = new Locker(1);
+        LockerBotManager lockerBotManager = new LockerBotManager(Collections.singletonList(locker));
+        lockerBotManager.storeBag();
+        assertNull(lockerBotManager.storeBag());
     }
 }
