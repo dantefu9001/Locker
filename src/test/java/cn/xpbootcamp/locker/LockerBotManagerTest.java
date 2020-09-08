4ddf59d42219d0e696a7ddedcc444e7f7565b8a2
zenan <zenan.fu@thoughtworks.com>
2020-09-08 23:19:53 +0800

    should success when ask smart bot to store bag

--- src/test/java/cn/xpbootcamp/locker/LockerBotManagerTest.java
+++ src/test/java/cn/xpbootcamp/locker/LockerBotManagerTest.java
@@ -25,12 +25,17 @@ public class LockerBotManagerTest {
     }
 
     @Test
-    public void should_success_when_ask_primary_bot_to_save() {
+    public void should_success_when_ask_other_bot_to_save() {
         Locker primaryLocker = new Locker(10);
+        Locker smartLocker = new Locker(10);
         Locker managerLocker = new Locker(10);
         PrimaryLockerBot primaryLockerBot = new PrimaryLockerBot(Collections.singletonList(primaryLocker));
+        SmartLockerBot smartLockerBot = new SmartLockerBot(Collections.singletonList(smartLocker));
         LockerBotManager lockerBotManager = new LockerBotManager(Collections.singletonList(managerLocker));
         assertNotNull(lockerBotManager.askBotToStoreBag(primaryLockerBot));
+        assertNotNull(lockerBotManager.askBotToStoreBag(smartLockerBot));
         Assert.assertEquals(primaryLocker.getFreeCapacity(), 9);
+        Assert.assertEquals(smartLocker.getFreeCapacity(), 9);
     }
+
 }
